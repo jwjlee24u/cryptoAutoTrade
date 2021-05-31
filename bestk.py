c7403b23757cf8df770c793c5af3f9668983afa2
jwjlee24u <jwjlee24u@gmail.com>
2021-05-31 15:50:15 +0900

    kvalue

--- bestk.py
+++ bestk.py
@@ -9,9 +9,13 @@ def get_ror(k=0.5):
     # df['range'] = (df['high'] - df['low']) * k
     # df['target'] = df['open'] + df['range'].shift(1)
     yesterday = today - datetime.timedelta(days = 1)
-    df = pyupbit.get_ohlcv("KRW-BTC", interval="minute60", count=17, to=str(yesterday)+" 17:00") #str(today)+" 18:00"
+    #setting up df for 1am ~ 8:00 trade
+    df = pyupbit.get_ohlcv("KRW-BTC", interval="minute60", count=8, to=str(yesterday)+" 9:00") #str(today)+" 18:00"
     # #from 9am add by "18" hours
+    #find the target value for the specific period
     target = df.iloc[-1]['close'] + (df['high'].max() - df['low'].min()) * k
+
+
     # df['range'] = (df['high'] - df['low']) * k
     # df['target'] = df['open'] + df['range'].shift(1)
     #print(df['target'])
@@ -24,11 +28,10 @@ def get_ror(k=0.5):
                          df.iloc[-1]['close'] / target,
                          #df['close'] / df['target'] - fee,
                          1)
-    print(df['high'].max(), df.iloc[-1]['close'], target)
     ror = df['ror'].cumprod()[-2]
     return ror
 
 
-for k in np.arange(0.1, 1.0, 0.01):
+for k in np.arange(0.1, 1.0, 0.02):
     ror = get_ror(k)
     print("%.1f %f" % (k, ror))
