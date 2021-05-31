c3c89fc09ac07d8b8d5454dc687d5f76471152cc
jwjlee24u <jwjlee24u@gmail.com>
2021-05-31 21:39:56 +0900

    update

--- bitcoinAutoTrade.py
+++ bitcoinAutoTrade.py
@@ -7,6 +7,8 @@ from fbprophet import Prophet
 #Access and Secret Key
 access = "wvwHae1CPfhgjBTugAm3xu3PijbpXEy0jWNj7vnI"
 secret = "w3lalAWfTAiv9NR6cgftAIJyAKFHVZHZaRgi39zl"
+end_hour1 = 7
+end_hour2 = 10
 
 def get_target_price(ticker, k):
     """변동성 돌파 전략으로 매수 목표가 조회"""
@@ -47,45 +49,78 @@ def prophet(ticker):
     forecast = model.predict(future)
     return [data, forecast]
 
-predicted_close_price = 0
-def predict_price(ticker):
-    global predicted_close_price
+predicted_close_price1 = 0
+def predict_price1(ticker):
+    global predicted_close_price1
     data = prophet(ticker)[0]
     forecast = prophet(ticker)[1]
-    closeDf = forecast[forecast['ds'] == forecast.iloc[-1]['ds'].replace(hour=9)]
+    closeDf = forecast[forecast['ds'] == forecast.iloc[-1]['ds'].replace(hour=end_hour1)]
     if len(closeDf) == 0:
-        closeDf = forecast[forecast['ds'] == data.iloc[-1]['ds'].replace(hour=9)]
+        closeDf = forecast[forecast['ds'] == data.iloc[-1]['ds'].replace(hour=end_hour1)]
     closeValue = closeDf['yhat'].values[0]
-    predicted_close_price = closeValue
-predict_price("KRW-BTC")
-schedule.every().hour.do(lambda: predict_price("KRW-BTC"))
+    predicted_close_price1 = closeValue
+predict_price1("KRW-BTC")
+schedule.every().hour.do(lambda: predict_price1("KRW-BTC"))
 
-def get_start_time(ticker):
+predicted_close_price2 = 0
+def predict_price2(ticker):
+    global predicted_close_price2
+    data = prophet(ticker)[0]
+    forecast = prophet(ticker)[1]
+    closeDf = forecast[forecast['ds'] == forecast.iloc[-1]['ds'].replace(hour=end_hour2)]
+    if len(closeDf) == 0:
+        closeDf = forecast[forecast['ds'] == data.iloc[-1]['ds'].replace(hour=end_hour2)]
+    closeValue = closeDf['yhat'].values[0]
+    predicted_close_price2 = closeValue
+predict_price2("KRW-BTC")
+schedule.every().hour.do(lambda: predict_price2("KRW-BTC"))
+
+def get_start_time1(ticker):
     """시작 시간 조회"""
     #df = pyupbit.get_ohlcv(ticker, interval="day", count=1)
-    df = pyupbit.get_daily_ohlcv_from_base("KRW-BTC", base=0)
+    df = pyupbit.get_daily_ohlcv_from_base(ticker, base=0)
     start_time = df.index[-1]
     return start_time
+
+def get_start_time2(ticker):
+    """시작 시간 조회"""
+    #df = pyupbit.get_ohlcv(ticker, interval="day", count=1)
+    df = pyupbit.get_daily_ohlcv_from_base(ticker, base=12)
+    start_time = df.index[-1]
+    return start_time
+
 
 
 # 자동매매 시작
 while True:
     try:
         now = datetime.datetime.now()
-        start_time = get_start_time("KRW-BTC") #start at midnight
-        end_time = start_time + datetime.timedelta(hours=16)
+        start_time1 = get_start_time1("KRW-BTC") #start at midnight
+        end_time1 = start_time1 + datetime.timedelta(hours=end_hour1)
+        start_time2 = get_start_time2("KRW-BTC") #start at midnight
+        end_time2 = start_time2 + datetime.timedelta(hours=end_hour2)
         schedule.run_pending()
-        print(start_time, end_time)
-        
-        if start_time < now < end_time - datetime.timedelta(seconds=10):
-            print("under if")
+        print("start1" + str(start_time1), "start2" + str(start_time2))
+        print("end1" + str(end_time1), "start2" + str(end_time2))
+
+        if start_time1 < now < end_time1 - datetime.timedelta(seconds=10):
+            print("under 1")
             target_price = get_target_price("KRW-BTC", 0.1)
             current_price = get_current_price("KRW-BTC")
-            if target_price < current_price and current_price < predicted_close_price:
+            if target_price < current_price and current_price < predicted_close_price1:
                 krw = get_balance("KRW")
                 if krw > 5000:
                     upbit.buy_market_order("KRW-BTC", krw*0.9995)
-                    print("bought")
+                    print("bought at 1")
+        elif start_time2 < now < end_time2 - datetime.timedelta(seconds=10):
+            print("under 2")
+            target_price = get_target_price("KRW-BTC", 0.1)
+            current_price = get_current_price("KRW-BTC")
+            if target_price < current_price and current_price < predicted_close_price2:
+                krw = get_balance("KRW")
+                if krw > 5000:
+                    upbit.buy_market_order("KRW-BTC", krw*0.9995)
+                    print("bought at 2")
         else:
             print("under else")
             btc = get_balance("BTC")
